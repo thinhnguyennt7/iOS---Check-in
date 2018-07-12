b172e53540e90d813790bce8a645f3480238ddc2
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2018-07-12 01:48:51 -0400

    Main home page login logic

--- checkin App/ViewController.swift
+++ checkin App/ViewController.swift
@@ -7,9 +7,49 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    // Login Page user input
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    // Login Button login methoh AUTH
+    @IBAction func loginAuth(_ sender: Any) {
+        
+        // In condition if the email and password not empty
+        if emailInput.text != "" && passwordInput.text != "" {
+            Auth.auth().signIn(withEmail: emailInput.text!, password: passwordInput.text!, completion: {(user, error) in
+                
+                // If the email and password correct
+                if user != nil {
+                    print("Sign in successfully")
+                    
+                    if self.segmentControl.selectedSegmentIndex == 0 {
+                        print("In Owner side")
+                        self.performSegue(withIdentifier: "owner_View", sender: self)
+                    } else {
+                        print("In Client side")
+                        self.performSegue(withIdentifier: "client_view", sender: self)
+                    }
+                    
+                // if not correct
+                } else {
+                    if let myError = error?.localizedDescription {
+                        print(myError)
+                    } else {
+                        print("ERROR")
+                    }
+                }
+            })
+        } else {
+            print("Email or Password can not be empty")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
