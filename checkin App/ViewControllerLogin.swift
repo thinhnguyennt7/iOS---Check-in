dded3fe0f4704659a4f6ad510ebb07f380484e44
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2018-07-14 13:21:36 -0400

    add featture to dimiss the keyboard when tap return or anywhere else

--- checkin App/ViewControllerLogin.swift
+++ checkin App/ViewControllerLogin.swift
@@ -11,7 +11,7 @@ import FirebaseAuth
 
 class ViewControllerLogin: UIViewController {
 
-    // Initlize the values
+    // INITLIZE THE VALUES
     @IBOutlet weak var pass_Input: UITextField!
     @IBOutlet weak var email_Input: UITextField!
     @IBOutlet weak var segmentControl: UISegmentedControl!
@@ -19,8 +19,9 @@ class ViewControllerLogin: UIViewController {
     @IBOutlet weak var error_Log: UILabel!
     
     
-    // Next button logic nethod AUTH
+    // NEXT BUTTON LOGIC NETHOD AUTH
     @IBAction func login_Button(_ sender: Any) {
+        view.endEditing(true)
         self.error_Log.text = ""
         if email_Input.text != "" && pass_Input.text != "" {
             Auth.auth().signIn(withEmail: email_Input.text!, password: pass_Input.text!, completion: {(user, error) in
@@ -54,13 +55,65 @@ class ViewControllerLogin: UIViewController {
         }
     }
     
-    // When page load
+    // WHEN PAGE LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
         self.error_Log.text = ""
+        configureTextFields()
+        configureTapGesture()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    // TEXTFIELD CONFIGUTE DELEGATE
+    private func configureTextFields() {
+        email_Input.delegate = self
+        pass_Input.delegate = self
+    }
+    
+    // TAPGESTURE CONFIGURE
+    private func configureTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewControllerLogin.handleTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    // WHEN WE TAP OUTSIDE THE KEYBOARD
+    @objc func handleTap() {
+        print("Handle tap was called")
+        view.endEditing(true)
+    }
+    
+    // AUTO SHIFT TO FIT THE KEYBOARD AND TEXTFIELD
+    func moveTextFiled(textField: UITextField, moveDistance: Int, up: Bool) {
+        let moveDuration = 0.3
+        let movement: CGFloat = CGFloat(up ? moveDistance : -moveDistance) // if statement
+        
+        UIView.beginAnimations("animateTextField", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(moveDuration)
+        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement)
+        UIView.commitAnimations()
+    }
+}
+
+// ABOUT THE TEXT FIELD
+extension ViewControllerLogin: UITextFieldDelegate {
+    
+    // KEYBOARD SHOWS
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        moveTextFiled(textField: textField, moveDistance: -250, up: true)
+    }
+    
+    // KEYBOARD HIDDEN
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        moveTextFiled(textField: textField, moveDistance: -250, up: false)
+    }
+    
+    // WHEN WE CLICK RETURN ON THE KEYBOARD, DIMISS THE KEYBOARD
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
---
+++ checkin App/keyboardViewShift.swift
@@ -0,0 +1,9 @@
+//
+//  keyboardViewShift.swift
+//  checkin App
+//
+//  Created by Thinh Nguyen on 7/14/18.
+//  Copyright © 2018 Thinh Nguyen. All rights reserved.
+//
+
+import Foundation
